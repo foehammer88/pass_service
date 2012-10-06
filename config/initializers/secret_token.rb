93c3cc96eb796c1c9c77aa6b909fbc1156fd3df8
Patrick Ganson <foehammer88@gmail.com>
2012-10-05 21:15:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PassService::Application.config.secret_token = '9f4e3d41ca1636b45a10d5484c912217c56b97053723fe3731cec9af8eceb05dd877a32f7403bc172a7aa962bda889fb38f836857603e6a93199ac3a5422fe09'
